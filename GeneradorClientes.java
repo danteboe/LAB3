6859a50065f23cf3d4c2eea27904d22ed175f936
Juan-David <jd.torresa1@uniandes.edu.co>
2025-02-07 15:05:27 -0500

    vaino

--- GeneradorClientes.java
+++ GeneradorClientes.java
@@ -1,3 +1,22 @@
-public class GeneradorClientes {
+public class GeneradorClientes extends Thread{
     
+    private Fila fila;
+
+    public GeneradorClientes(Fila pFila){
+        this.fila = pFila;
+    }
+
+    @Override
+    public void run(){
+        int uidActual = 0;
+        while(uidActual!=100){
+            fila.anadirCliente(uidActual);
+            uidActual++;
+            try {
+                Thread.sleep(2000);
+            } catch (Exception e) {
+            }
+        }
+    }
+
 }
