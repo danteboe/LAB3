5ba99c8026861be3de4d9fe2d4acaa74a1d44200
Juan-David <jd.torresa1@uniandes.edu.co>
2025-02-07 15:18:37 -0500

    vaino

--- Cajero.java
+++ Cajero.java
@@ -1,9 +1,12 @@
+
+import java.util.HashMap;
+
 public class Cajero extends Thread {
     private int id;
-    private int[] filaClientes;
+    private Fila filaClientes;
     private double factorDeCansancio;
 
-    public Cajero(int id, int[] filaClientes) {
+    public Cajero(int id, Fila filaclientes) {
         this.id = id;
         this.filaClientes = filaClientes;
         this.factorDeCansancio = 1.0;
@@ -17,33 +20,33 @@ public class Cajero extends Thread {
         return id;
     }
 
-    public int[] getFilaClientes() {
+    public Fila getFilaClientes() {
         return filaClientes;
     }
 
     @Override
     public void run() {
-        System.out.println("El cajero " + id + " comienza a procesar la fila " + id);
-        
-        for (int i = 0; i < filaClientes.length; i++) {
-            int tiempoBase = filaClientes[i];
-            double tiempoCalculado = tiempoBase * factorDeCansancio;
+        while(true){System.out.println("El cajero " + id + " comienza a procesar la fila " + id);
+        HashMap<Integer, Integer> clienteActual = filaClientes.sacarCliente();
+        int tiempoBase = clienteActual.values().iterator().next();
+        int idCliente = clienteActual.keySet().iterator().next();
+        double tiempoCalculado = tiempoBase * factorDeCansancio;
 
-            System.out.println("El cajero " + id + " atendiendo al cliente " + (i + 1) + 
-                               " de la fila " + id + 
-                               " (tiempo de procesamiento básico: " + tiempoBase + "ms) " +
-                               "factor de cansancio actual: " + factorDeCansancio + 
-                               " tiempo de procesamiento actual: " + tiempoCalculado + "ms");
+        System.out.println("El cajero " + id + " atendiendo al cliente " + (idCliente) + 
+                           " de la fila " + id + 
+                           " (tiempo de procesamiento básico: " + tiempoBase + "ms) " +
+                           "factor de cansancio actual: " + factorDeCansancio + 
+                           " tiempo de procesamiento actual: " + tiempoCalculado + "ms");
 
-            try {
-                Thread.sleep((long) tiempoCalculado);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            factorDeCansancio += tiempoBase * 0.001; 
+        try {
+            Thread.sleep((long) tiempoCalculado);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("El cajero " + id + " ha terminado de atender.");
+        factorDeCansancio += tiempoBase * 0.001; 
+    
+    System.out.println("El cajero " + id + " ha terminado de atender.");}
+        
     }
 }
