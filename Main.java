95d803217bb5d39e1beb96660b65825bf84cf2a9
Felipe Andrés Mesa Niño <f.mesan@uniandes.edu.co>
2025-02-07 15:16:26 -0500

    Cambios en Main

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
-    
+    public static void main(String[] args){
+        
+        int numCajeros = 5;
+        int numClientes = 10;
+
+        Fila fila = new Fila();
+        GeneradorClientes clientes = new GeneradorClientes(fila, numClientes);
+
+        for (int i = 0; i < numCajeros; i++)
+            Cajero cajero = new Cajero(i, fila);
+    }
 }
